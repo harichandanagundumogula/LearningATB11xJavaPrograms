94bae3601dfc99185ab2c8256f516fac19eae339
Harichandana <harichandanagundumogula@gmail.com>
2025-04-03 16:46:52 +0530

    April 3

---
+++ src/learningATB11xJavaPrograms/Lab004_comments.java
@@ -0,0 +1,17 @@
+package learningATB11xJavaPrograms;
+
+public class Lab004_comments {
+    public static void main(String[] args) {
+        System.out.println("comments");
+        //comments this line is ignored
+        // this is single line comment
+        /* these comments are
+        multiple line comments*/
+        /**
+         * comments
+         * multiple
+         * multiple comments
+         **/
+
+    }
+}
---
+++ src/learningATB11xJavaPrograms/Lab005_shortcut.java
@@ -0,0 +1,13 @@
+package learningATB11xJavaPrograms;
+
+public class Lab005_shortcut {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        //main -main funtion
+        //sout - system.out.println
+        //ctrl
+       // ctrl+d - duplicate
+        //ctrl+/ -comment
+    }
+}
